8ae3ac58f133ee70dd2fcad458e3510d88cf7054
Aakash Sharma <aakashvats2910@gmail.com>
2020-02-17 18:41:17 +0530

    After a long time

--- app/src/main/java/com/example/rental/MainActivity.kt
+++ app/src/main/java/com/example/rental/MainActivity.kt
@@ -1,73 +1,69 @@
 package com.example.rental
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputFilter
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
 import android.widget.Toast
 import com.example.rental.cloud.Fire
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
+import javax.xml.transform.Result
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        startActivity(Intent(this, UserInfoGatherActivity::class.java))
+
         Fire.Static.initializeCloud()
 
-        setBackButtonFunctionality()
-        setMobileTextFieldLength()
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.PhoneBuilder().build()
+        )
 
-        send_otp_image_view.setOnClickListener{
-            if (mobile_edit_text.text.toString().length == 10) {
-                send_otp_image_view.isEnabled = false
-                sendOTP("+91" + mobile_edit_text.text.toString())
+        val ss = SpannableString("Packed your bags!\nWhy go find door to door when we are here to help you.\nJust see the apartment and decide for yourself.")
+        ss.setSpan(RelativeSizeSpan(2f), 0,17,0)
+        ss.setSpan(ForegroundColorSpan(Color.RED), 0, 16, 0)
+        ss.setSpan(ForegroundColorSpan(Color.BLUE), 16, 17, 0)
+        homepage_text_view.text = ss
+
+        start_using_button.setOnClickListener{
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+
             } else {
-                Snackbar.make(it, "Not a valid number!", 2000).show()
+
             }
         }
-
-    }
-
-    // Method to send the OTP to the number.
-    private fun sendOTP(phoneNumber: String) {
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-            phoneNumber, // Phone number to verify
-            60, // Timeout duration
-            TimeUnit.SECONDS, // Unit of timeout
-            this, // Activity (for callback binding)
-            setCallback()) // OnVerificationStateChangedCallbacks
-    }
-
-    // Making a callback for the Authentication Provider for the OTP functionality.
-    private fun setCallback(): PhoneAuthProvider.OnVerificationStateChangedCallbacks {
-        return object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-            override fun onVerificationCompleted(p0: PhoneAuthCredential) {
-                println("()()()() OTP SENT")
-                startActivity(Intent(this@MainActivity, OTPActivity::class.java))
-            }
-
-            override fun onVerificationFailed(p0: FirebaseException) {
-                Toast.makeText(applicationContext, p0.message, Toast.LENGTH_LONG).show()
-                send_otp_image_view.isEnabled = true
-            }
-        }
-    }
-
-    private fun setBackButtonFunctionality() {
-        back_main_activity.setOnClickListener{
-            finish()
-        }
-    }
-
-    private fun setMobileTextFieldLength() {
-        mobile_edit_text.filters = arrayOf(InputFilter.LengthFilter(10))
     }
 
 }
---
+++ app/src/main/java/com/example/rental/UserInfoGatherActivity.kt
@@ -0,0 +1,70 @@
+package com.example.rental
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.SpinnerAdapter
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_user_info_gather.*
+
+class UserInfoGatherActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private var isMale: Boolean? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info_gather)
+
+        male.setOnClickListener{
+            male.setBackgroundResource(R.drawable.selected_background)
+            male.setTextColor(Color.parseColor("#ffffff"))
+            female.setBackgroundResource(R.drawable.non_selected_button)
+            female.setTextColor(Color.parseColor("#00B0FF"))
+            isMale = true
+        }
+
+        female.setOnClickListener{
+            female.setBackgroundResource(R.drawable.selected_background)
+            female.setTextColor(Color.parseColor("#ffffff"))
+            male.setBackgroundResource(R.drawable.non_selected_button)
+            male.setTextColor(Color.parseColor("#00B0FF"))
+            isMale = false
+        }
+
+        next_page_button.setOnClickListener{
+//            if (isMale != null && first_name.text.toString().isNotEmpty() && last_name.text.toString().isNotEmpty())
+        }
+
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            photo_view.setImageBitmap(imageBitmap)
+        }
+    }
+
+    private fun isCameraPermissionGranted(): Boolean {
+        return (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+    }
+
+}
